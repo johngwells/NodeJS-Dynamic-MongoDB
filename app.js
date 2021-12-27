4a350baba0f1d36b89c97909b08c734be7cdbd95
John Wells <molekewl2017@gmail.com>
2021-12-26 23:11:32 -0800

    create relation 1to1 user to cart | addToCart adds only 1 item of a product with a quantity update & the updated cart data back into mongodb

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('61c81785b7e0a6b835ee6766')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
